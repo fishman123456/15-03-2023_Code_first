f67f6fe7440fdf1ead35331792ad7be43d758b1c
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-15 21:00:35 +0700

    добавили пользователей

--- Program.cs
+++ Program.cs
@@ -2,4 +2,18 @@
 using _15_03_2023_Code_first;
 using Microsoft.EntityFrameworkCore;
 
-using (Human_Context db = new Human_Context()) ;
+using (Human_Context db = new Human_Context()) 
+{
+    db.Humans.Add(new Human("Олеся", "Крестикова", "женский", 36));
+    db.SaveChanges();
+}
+
+//using (Human_Context db = new Human_Context())
+//{
+//    var data = db.Humans.ToList();
+
+//    foreach (Human human in data)
+//    {
+//        Console.WriteLine(human +"\n");
+//    }
+//}
