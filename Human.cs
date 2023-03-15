2cedb7ef1e2b9399080ee588978b2823d31ce984
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-15 20:01:13 +0700

    Initial commit

---
+++ Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_03_2023_Code_first
+{
+    internal class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Fname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Human (string _name, string _fname, string _gender, int _age ) 
+        {
+        Name = _name;
+            Fname = _fname;
+            Gender = _gender;
+            Age = _age;
+        }
+        public override string ToString()
+        {
+            return "Имя "+Name+" Фамилия "+ Fname + " Пол " + Gender + " Возраст " + Age;
+        }
+
+    }
+}
---
+++ Human_Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_03_2023_Code_first
+{
+    internal class Human_Context: DbContext
+
+    {
+        public DbSet<Human> Human { get; set; } = null!;
+
+        public Human_Context() 
+        {
+        Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["Human_Base"].ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+    }
+}
